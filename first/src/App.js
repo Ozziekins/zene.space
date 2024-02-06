cb72092cbf5bd1effc9fad6af13885cb7f28cc41
Ozziekins <o.okonicha@innopolis.university>
2024-02-06 14:26:58 +0300

    my first create react app

---
+++ first/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>hi from Ozzie</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
